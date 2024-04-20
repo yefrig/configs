69e27793dc33f1723c297631e9c264d3629d6589
Yefri Gaitan <yefrigaitan@gmail.com>
2024-04-20 18:05:30 -0400

    use preferred keybinds for completion

--- terminal/.config/nvim/init.lua
+++ terminal/.config/nvim/init.lua
@@ -399,10 +399,8 @@ require("lazy").setup({
 				--
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
-					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 					-- Scroll the documentation window [b]ack / [f]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
