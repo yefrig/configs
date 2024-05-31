7ebf7a4985544cecee4ba093e3c13acd62a4fd33
Yefri Gaitan <yefrigaitan@gmail.com>
2024-05-31 19:13:49 -0400

    use random ascii art for dashboard header

---
+++ terminal/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    dependencies = {
+      "MaximilianLloyd/ascii.nvim",
+    },
+    opts = function(_, opts)
+      local ascii = require("ascii")
+      if opts.config == nil then
+        opts.config = {}
+      end
+      opts.config.header = ascii.get_random_global()
+    end,
+  },
+}
