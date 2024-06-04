13ca69e75b264ff889b2481fd6173a00f4f3ef91
Yefri Gaitan <yefrigaitan@gmail.com>
2024-06-03 23:19:58 -0400

    add oil.nvim

---
+++ terminal/.config/nvim/lua/plugins/files.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "-",
+        "<CMD>Oil<CR>",
+        desc = "Open parent directory",
+      },
+    },
+  },
+}
