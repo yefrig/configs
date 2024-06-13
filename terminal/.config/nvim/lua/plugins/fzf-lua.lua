c8e008c65438bad62eae23ade81b75e3884cfc19
Yefri Gaitan <yefrigaitan@gmail.com>
2024-06-13 00:01:54 -0400

    enable fzf extra but keep my keymaps

--- terminal/.config/nvim/lua/plugins/fzf-lua.lua
+++ terminal/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,14 +1,7 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    enabled = false,
-  },
-  {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      "default",
-    },
     keys = {
       { "<C-\\>", "<Cmd>FzfLua buffers<CR>", desc = "Fzf: buffers" },
       { "<C-k>", "<Cmd>FzfLua builtin<CR>", desc = "Fzf: builtin" },
