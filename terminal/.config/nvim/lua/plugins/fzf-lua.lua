8e545a15c59e15e275b5480f31952f50fb3ff913
Yefri G <yefrigaitan@gmail.com>
2024-06-18 22:50:50 -0400

    fix conflicting keymap and allow fzf to cycle

--- terminal/.config/nvim/lua/plugins/fzf-lua.lua
+++ terminal/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,12 +1,16 @@
 return {
   {
     "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      fzf_opts = {
+        ["--cycle"] = true,
+      },
+    },
     keys = {
       { "<C-\\>", "<Cmd>FzfLua buffers<CR>", desc = "Fzf: buffers" },
       { "<C-k>", "<Cmd>FzfLua builtin<CR>", desc = "Fzf: builtin" },
       { "<C-p>", "<Cmd>FzfLua files<CR>", desc = "Fzf: files" },
-      { "<C-l>", "<Cmd>FzfLua lgrep_curbuf<CR>", desc = "Fzf: live grep buffer" },
+      { "<C-/>", "<Cmd>FzfLua lgrep_curbuf<CR>", desc = "Fzf: live grep buffer" },
       { "<C-g>", "<Cmd>FzfLua grep_project<CR>", desc = "Fzf: grep project" },
     },
   },
