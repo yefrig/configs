49cd1cc040d3901047554f9acc9860723ef93c14
Yefri G <yefrigaitan@gmail.com>
2024-08-17 13:41:09 -0400

    Add missing settings for gd and grr keybinds for better QOL

--- terminal/.config/nvim/lua/plugins/fzf-lua.lua
+++ terminal/.config/nvim/lua/plugins/fzf-lua.lua
@@ -13,13 +13,28 @@ return {
       opts = {
         mappings = {
           n = {
-            ["<C-k>"] = {
+            ["<C-p>"] = {
               function() require("fzf-lua").builtin() end,
               desc = "Fzf: builtin",
+            },
+            grr = {
+              function() require("fzf-lua").lsp_references { jump_to_single_result = true, ignore_current_line = true } end,
+              desc = "Show references",
             },
           },
         },
       },
+    },
+    {
+      "AstroNvim/astrolsp",
+      -- I don't know why providing an opts table drops the desc and cond keys of a mapping that is already defined
+      opts = function(_, opts)
+        if opts.mappings.n.gd then
+          opts.mappings.n.gd[1] = function()
+            require("fzf-lua").lsp_definitions { jump_to_single_result = true, ignore_current_line = true }
+          end
+        end
+      end,
     },
   },
 }
