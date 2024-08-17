a5b683778d8073b9e177917490c979bed83a9c55
Yefri G <yefrigaitan@gmail.com>
2024-08-16 21:44:40 -0400

    Add smart-splits.nvim integration to wezterm

--- terminal/.wezterm.lua
+++ terminal/.wezterm.lua
@@ -1,7 +1,18 @@
 local wezterm = require("wezterm")
 
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+
 local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine"
+
+-- integration with neovim plugin
+smart_splits.apply_to_config(config, {
+	direction_keys = { "h", "j", "k", "l" },
+	modifiers = {
+		move = "CTRL",
+		resize = "META",
+	},
+})
 
 return config
