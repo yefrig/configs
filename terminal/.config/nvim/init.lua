eb80a44cc0b2e772c780149b133a4c583dd10ffc
Yefri G <yefrigaitan@gmail.com>
2024-09-23 14:52:59 -0400

    New setup with Mini.deps and Mini.basics

---
+++ terminal/.config/nvim/init.lua
@@ -0,0 +1,33 @@
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath("data") .. "/site/"
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
+if not vim.loop.fs_stat(mini_path) then
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/echasnovski/mini.nvim",
+		mini_path,
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd("packadd mini.nvim | helptags ALL")
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+require("mini.deps").setup({ path = { package = path_package } })
+
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+now(function()
+	vim.cmd.colorscheme("randomhue")
+end)
+
+-- TODO: might want to disable helper symbols but keep extra_ui goodies enabled
+now(function()
+	require("mini.basics").setup({
+		options = { extra_ui = true },
+		mappings = { windows = true, move_with_alt = true },
+		autocommands = { relnum_in_visual_mode = true },
+	})
+end)
