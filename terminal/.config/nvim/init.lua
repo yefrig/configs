4e898f96b588b7dabb60da2f77916cec156f4f31
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2024-11-14 17:12:33 -0500

    Add initial MiniDeps setup and MiniBasics (settings, mappings, autocmds)

---
+++ terminal/.config/nvim/init.lua
@@ -0,0 +1,28 @@
+pcall(function() vim.loader.enable() end)
+
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+require('mini.deps').setup({ path = { package = path_package } })
+
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+now(function() vim.cmd('colorscheme randomhue') end)
+
+now(function()
+  require('mini.basics').setup({
+    mappings = { windows = true, move_with_alt = true },
+    autocommands = { relnum_in_visual_mode = true },
+  }) 
+end)
