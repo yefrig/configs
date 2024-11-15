9d53785988330f37d761d6489ab9525239923a82
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2024-11-15 00:47:30 -0500

    Add key clues

--- terminal/.config/nvim/init.lua
+++ terminal/.config/nvim/init.lua
@@ -121,6 +121,62 @@ later(function() require('mini.pairs').setup() end)
 later(function() require('mini.indentscope').setup() end)
 later(function() require('mini.diff').setup() end)
 later(function() require('mini.git').setup() end)
+later(function()
+  local miniclue = require('mini.clue')
+  require('mini.clue').setup({
+    triggers = {
+      -- Leader triggers
+      { mode = 'n', keys = '<Leader>' },
+      { mode = 'x', keys = '<Leader>' },
+
+      -- Built-in completion
+      { mode = 'i', keys = '<C-x>' },
+
+
+      -- mini.basics
+      { mode = 'n', keys = [[\]] },
+
+      -- mini.bracketed
+      { mode = 'n', keys = '[' },
+      { mode = 'n', keys = ']' },
+      { mode = 'x', keys = '[' },
+      { mode = 'x', keys = ']' },
+
+      -- `g` key
+      { mode = 'n', keys = 'g' },
+      { mode = 'x', keys = 'g' },
+
+      -- Marks
+      { mode = 'n', keys = "'" },
+      { mode = 'n', keys = '`' },
+      { mode = 'x', keys = "'" },
+      { mode = 'x', keys = '`' },
+
+      -- Registers
+      { mode = 'n', keys = '"' },
+      { mode = 'x', keys = '"' },
+      { mode = 'i', keys = '<C-r>' },
+      { mode = 'c', keys = '<C-r>' },
+
+      -- Window commands
+      { mode = 'n', keys = '<C-w>' },
+
+      -- `z` key
+      { mode = 'n', keys = 'z' },
+      { mode = 'x', keys = 'z' },
+    },
+
+    clues = {
+      { mode = 'n', keys = '<Leader>g', desc = 'Git' },
+      miniclue.gen_clues.builtin_completion(),
+      miniclue.gen_clues.g(),
+      miniclue.gen_clues.marks(),
+      miniclue.gen_clues.registers(),
+      miniclue.gen_clues.windows(),
+      miniclue.gen_clues.z(),
+    },
+  })
+end)
 
 
 -- keymaps
