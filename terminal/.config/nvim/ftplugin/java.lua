332141f22b4af38dab6b503c4d4ca43d561e0357
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2024-12-02 15:54:53 -0500

    Simplify jdtls setup

--- terminal/.config/nvim/ftplugin/java.lua
+++ terminal/.config/nvim/ftplugin/java.lua
@@ -3,32 +3,20 @@ local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. p
 vim.fn.mkdir(workspace_dir, "p")
 
 local cpm_capabilities = require('blink.cmp').get_lsp_capabilities()
+local root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"})
 
 local config = {
   capabilities = cpm_capabilities,
   on_attach = require('lsp_utils').on_attach,
   cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xms1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-jar",
     -- point to jdtls installation
-    vim.fn.expand "~/projects/jdt-language-server-1.42.0-202411281516/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
+    vim.fn.expand "~/projects/jdt-language-server-1.42.0-202411281516/bin/jdtls",
     "-configuration",
-    vim.fn.expand "~/projects/jdt-language-server-1.42.0-202411281516/config_linux/",
+    vim.fn.expand "~/.cache/jdtls",
     "-data",
     workspace_dir,
   },
-  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  root_dir = root_dir,
   settings = {
     java = {
       eclipse = { downloadSources = true },
