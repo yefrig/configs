105c2c973329c0715a7ea6859e55990dba276df5
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2025-01-28 19:55:44 -0500

    Reorder integrations so that /opt path is added before

--- terminal/.config/fish/config.fish
+++ terminal/.config/fish/config.fish
@@ -8,13 +8,13 @@ setenv GHCUP_INSTALL_BASE_PREFIX $HOME
 # fzf opts
 setenv FZF_DEFAULT_COMMAND 'fd --type f'
 
-# integrations
-starship init fish | source
-fzf --fish | source
-
 # path
 fish_add_path /opt/local/bin/
 fish_add_path $HOME/.cabal/bin
 fish_add_path $HOME/.ghcup/bin
+
+# integrations
+starship init fish | source
+fzf --fish | source
 
 fish_config theme choose "Rosé Pine"
