3bbfc8bdbefe71fd1e863df49ea6aead84c5d03d
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2025-02-06 11:36:12 -0500

    Only show virtual lines in the current_line

--- terminal/.config/nvim/lua/lsp_utils.lua
+++ terminal/.config/nvim/lua/lsp_utils.lua
@@ -3,7 +3,7 @@ local M = {}
 M.on_attach = function(_, buf_id)
   vim.lsp.inlay_hint.enable(true)
   vim.lsp.codelens.refresh( { bufnr = buf_id })
-  vim.diagnostic.config({ virtual_lines = true })
+  vim.diagnostic.config({ virtual_lines = { current_line = true } })
 
   vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
     buffer = buf_id,
