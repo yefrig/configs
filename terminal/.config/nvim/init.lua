428a3e0f5e08f89c1a5930ade30ff91bbdea7ccf
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2025-02-12 19:23:16 -0500

    Add localleader and cmdheight opts

--- terminal/.config/nvim/init.lua
+++ terminal/.config/nvim/init.lua
@@ -3,7 +3,7 @@ pcall(function() vim.loader.enable() end)
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
@@ -19,6 +19,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ","
+
+vim.opt.cmdheight = 0 -- hide command line unless needed
 
 vim.keymap.set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
