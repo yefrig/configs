840344b31b0e657424473f7265869adb5761cadd
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2025-03-10 13:17:20 -0400

    Add astro opts

--- terminal/.config/nvim/lua/astro.lua
+++ terminal/.config/nvim/lua/astro.lua
@@ -1,6 +1,30 @@
 return {
   -- branches are only needed until v5 is released for astronvim
-  { "AstroNvim/astrocore", version = false, branch = "v2" },
-  { "AstroNvim/astrolsp",  version = false, branch = "v3" },
-  { "AstroNvim/astroui",   version = false, branch = "v3" },
+  {
+    "AstroNvim/astrocore",
+    version = false,
+    branch = "v2",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        opt = {
+          relativenumber = false,
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    version = false,
+    branch = "v3",
+    ---@type AstroLSPOpts
+    opts = { features = { inlay_hints = true } }
+  },
+  {
+    "AstroNvim/astroui",
+    version = false,
+    branch = "v3",
+    ---@type AstroUIOpts
+    opts = { colorscheme = "astrodark" }
+  },
 }
