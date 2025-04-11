67e842eaafd3835f42ff9e69efc15c3dc77ac9f9
Yefri Gaitan <34199474+yefrig@users.noreply.github.com>
2025-04-10 23:35:44 -0400

    Use picker for lsp mappings

--- terminal/.config/nvim/lua/astro.lua
+++ terminal/.config/nvim/lua/astro.lua
@@ -1,3 +1,4 @@
+local methods = vim.lsp.protocol.Methods
 return {
   -- branches are only needed until v5 is released for astronvim
   {
@@ -14,7 +15,33 @@ return {
   {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
-    opts = { features = { inlay_hints = true } }
+    opts = {
+      features = { inlay_hints = true },
+      mappings = {
+        n = {
+          gd = {
+            function() Snacks.picker.lsp_definitions() end,
+            desc = 'Goto Definition',
+            cond = methods.textDocument_definition
+          },
+          gD = {
+            function() Snacks.picker.lsp_declarations() end,
+            desc = 'Goto Declaration',
+            cond = methods.textDocument_declaration
+          },
+          grr = {
+            function() Snacks.picker.lsp_references() end,
+            desc = 'Goto References',
+            cond = methods.textDocument_references
+          },
+          gri = {
+            function() Snacks.picker.lsp_implementations() end,
+            desc = 'Goto Implementation',
+            cond = methods.textDocument_implementation
+          }
+        }
+      }
+    }
   },
   {
     "AstroNvim/astroui",
@@ -29,5 +56,10 @@ return {
       }
     }
   },
-  { "nvim-neo-tree/neo-tree.nvim", opts = { filesystem = { group_empty_dirs = true } } }
+  { "nvim-neo-tree/neo-tree.nvim", opts = { filesystem = { group_empty_dirs = true } } },
+  {
+    "folke/lazydev.nvim",
+    -- library is already setup to be extended by astronvim
+    opts = { library = { { path = "snacks.nvim", words = { "Snacks" } } } },
+  },
 }
